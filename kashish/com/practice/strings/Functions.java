a39cb278d47a37aeae23e12f7e163b2b8fee13f7
ishmeet-droid <ishmeetsingh477@gmail.com>
2023-09-16 18:04:51 +0530

    wrapper and string implementations

---
+++ kashish/com/practice/strings/Functions.java
@@ -0,0 +1,32 @@
+package kashish.com.practice.strings;
+import java.util.*;
+
+public class Functions {
+  
+    
+        public static void main(String args[]){
+    
+          String s1 = "abcdf ghi";
+          String s2 = "   abcdef  ";
+    
+          //trim...
+        //  String s3 = s2.trim();
+        //   String[] s = s1.split("\\s+");
+        //   String[] s4 = s1.split("");
+        //   for(String i : s){
+        //     System.out.println(i);
+        //   }
+        //   String joined = String.join(", ", s);
+        //   System.out.println(joined);
+
+        //    char[] chArr = s1.toCharArray();
+        //    for(char c : chArr){
+        //     System.out.println(c);
+        //    }
+           System.out.println(s1.indexOf('c'));
+           System.out.println(s1.indexOf("cd"));
+       
+        }
+
+     
+}
---
+++ kashish/com/practice/wrapper/Example.java
@@ -0,0 +1,13 @@
+package kashish.com.practice.wrapper;
+
+public class Example {
+
+     public static void main(String args[]){
+    
+        //Integer i = Integer.valueOf("122");
+       // Integer i = Integer.parseInt("12233.0001");
+        Float f = Float.parseFloat("12334");
+        System.out.println(f);
+        }
+    
+}
