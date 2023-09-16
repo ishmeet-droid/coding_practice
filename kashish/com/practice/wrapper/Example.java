c2b3ebc5f965b401933f4a5c359863d35b8d1f87
ishmeet-droid <ishmeetsingh477@gmail.com>
2023-09-16 18:05:15 +0530

    corrected

--- kashish/com/practice/wrapper/Example.java
+++ kashish/com/practice/wrapper/Example.java
@@ -5,7 +5,7 @@ public class Example {
      public static void main(String args[]){
     
         //Integer i = Integer.valueOf("122");
-       // Integer i = Integer.parseInt("12233.0001");
+       // Integer i = Integer.parseInt("12233");
         Float f = Float.parseFloat("12334");
         System.out.println(f);
         }
