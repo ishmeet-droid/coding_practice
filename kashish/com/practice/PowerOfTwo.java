c2ac2c36d41f634c94d27f0a83aa877997b67043
ishmeet-droid <ishmeetsingh477@gmail.com>
2023-09-16 12:05:45 +0530

    Power of two program less than a number.

---
+++ kashish/com/practice/PowerOfTwo.java
@@ -0,0 +1,25 @@
+package kashish.com.practice;
+
+public class PowerOfTwo {
+
+    static int findLargestPowerOfTwo(int n) {
+
+        if(n == 0){
+            return 0;
+        }
+  
+        int i;
+        for(i=0;Math.pow(2,i) <= n;i++);
+        return i-1; 
+      }
+  
+      public static void main(String args[]) {
+  
+          assert findLargestPowerOfTwo(9) == 3 : "Expect 3 for n = 9";
+  
+          assert findLargestPowerOfTwo(32) == 5 : "Expect 5 for n = 32";
+          System.out.println("All test cases in main function passed");
+  
+      }
+    
+}
